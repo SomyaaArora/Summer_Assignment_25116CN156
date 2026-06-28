8efbe969cd4bc7e80b43e26467facd6681906c01
SomyaaArora <somyaa3086@gmail.com>
2026-06-29 03:27:19 +0530

    Day-8 assignment is uploaded

---
+++ Day-8/Q30.cpp
@@ -0,0 +1,15 @@
+// To print number triangle
+#include<bits/stdc++.h>
+using namespace std;
+int main(){ 
+    int i,j;//initialising variables
+    for(i=1;i<=5;i++)//outer loop
+    {
+        for (j=1;j<=i;j++)//inner loop
+        {
+            cout << j ;
+        }
+        cout << "\n";
+    }
+}
+
